36502c6abc46e5975c484586b4ec381721f17ed0
Fabiola Burgos <fburgos913@gmail.com>
2026-04-05 19:29:00 -0700

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Módulo que contiene una función para concatenar dos cadenas de texto.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatena dos cadenas de texto y devuelve el resultado.
+    """
+    return str1 + str2
