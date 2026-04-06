191106cf920a06822a5e360ba2e7b0be881339f1
Fabiola Burgos <fburgos913@gmail.com>
2026-04-05 19:49:43 -0700

    Complex types - list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to sum a list of floats.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums all elements in a list of floats and returns the result.
+    """
+    return sum(input_list)
