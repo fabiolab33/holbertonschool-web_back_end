4644e6f765ca7deddda8405e907de4adb60ff563
Fabiola Burgos <fburgos913@gmail.com>
2026-04-05 19:25:43 -0700

    Basic annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Modulo que contiene una función para sumar dos números flotantes.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Suma dos números flotantes y devuelve el resultado.
+    """
+    return a + b
