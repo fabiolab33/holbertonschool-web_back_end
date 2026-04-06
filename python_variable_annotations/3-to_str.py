2bc59fe1bfa45476e817bf5a663578e7843ff511
Fabiola Burgos <fburgos913@gmail.com>
2026-04-05 19:33:47 -0700

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Módulo que contiene una función para convertir un flotante a cadena.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Devuelve la representación en cadena de un número flotante.
+    """
+    return str(n)
