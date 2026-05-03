63f944df7d7e5e98953af94572f3e0e818a8a330
Fabiola Burgos <fburgos913@gmail.com>
2026-05-03 12:03:52 -0500

    Primitive conversion

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,40 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  // size
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    this._size = value;
+  }
+
+  // location
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = value;
+  }
+
+  // Numeric conversion
+  valueOf() {
+    return this._size;
+  }
+
+  // String conversion
+  toString() {
+    return this._location;
+  }
+}
