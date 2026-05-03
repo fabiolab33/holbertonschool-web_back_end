87043c3c61f34a2b153e4a7b5bd441e35aedaa37
Fabiola Burgos <fburgos913@gmail.com>
2026-05-03 11:43:50 -0500

    Pricing class with static method

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Instance method
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
