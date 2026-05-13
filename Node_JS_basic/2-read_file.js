7ebdaef34bd355a3f9c3cb0b231e349512ada022
Fabiola Burgos <fburgos913@gmail.com>
2026-05-12 22:38:33 -0700

    Read and process CSV synchronously

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data
+      .split('\n')
+      .filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1); // remove header
+
+    const fields = {};
+    let total = 0;
+
+    students.forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
+
+      if (firstname && field) {
+        total += 1;
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      }
+    });
+
+    console.log(`Number of students: ${total}`);
+
+    for (const field in fields) {
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
