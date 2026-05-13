8d4fe7ee13135e884efb58dc29f87dbc93a76691
Fabiola Burgos <fburgos913@gmail.com>
2026-05-12 22:07:03 -0700

    Start set up for project Node_JS_basic

---
+++ Node_JS_basic/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
